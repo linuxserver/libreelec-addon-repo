bd78dc80731a74cb699371846da543d9a7be1a4b
aptalca <aptalca@users.noreply.github.com>
2019-02-07 16:05:13 -0500

    letsencrypt: multi-arch, add duckdns, wait for settings (#49)
    * letsencrypt: multi-arch, add duckdns, wait for settings
    
    * letsencrypt fix initial settings window

--- docker.linuxserver.letsencrypt/default.py
+++ docker.linuxserver.letsencrypt/default.py
@@ -9,14 +9,12 @@ class Monitor(xbmc.Monitor):
    def __init__(self, *args, **kwargs):
       xbmc.Monitor.__init__(self)
       self.id = xbmcaddon.Addon().getAddonInfo('id')
+      E_url_ = xbmcaddon.Addon().getSetting('E_url')
+      if E_url_ == "":
+         xbmcaddon.Addon().openSettings('id')
 
    def onSettingsChanged(self):
       subprocess.call(['systemctl', 'restart', self.id])
-
-E_url_ = xbmcaddon.Addon().getSetting('E_url')
-
-if E_url_ == "":
-   xbmcaddon.Addon().openSettings('id')
 
 if __name__ == '__main__':
    Monitor().waitForAbort()
