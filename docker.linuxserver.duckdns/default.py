7569b87f03b0c8181fe99938e2607ada73286bcc
aptalca <aptalca@users.noreply.github.com>
2019-02-19 16:59:05 -0500

    update various addons to use multi-arch builds

--- docker.linuxserver.duckdns/default.py
+++ docker.linuxserver.duckdns/default.py
@@ -9,14 +9,12 @@ class Monitor(xbmc.Monitor):
    def __init__(self, *args, **kwargs):
       xbmc.Monitor.__init__(self)
       self.id = xbmcaddon.Addon().getAddonInfo('id')
+      E_TOKEN_ = xbmcaddon.Addon().getSetting('E_TOKEN')
+      if E_TOKEN_ == "token":
+         xbmcaddon.Addon().openSettings('id')
 
    def onSettingsChanged(self):
       subprocess.call(['systemctl', 'restart', self.id])
-
-E_TOKEN_ = xbmcaddon.Addon().getSetting('E_TOKEN')
-
-if E_TOKEN_ == "token":
-   xbmcaddon.Addon().openSettings('id')
 
 if __name__ == '__main__':
    Monitor().waitForAbort()
